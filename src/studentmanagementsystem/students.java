6207c4819f18f3b48c3e83c1b5699b8793d58224
kyle2 <kyle2@KylesLaptop>
2026-01-08 15:24:03 -0600

    Implemented changing students information including schedule & grades

--- src/studentmanagementsystem/students.java
+++ src/studentmanagementsystem/students.java
@@ -15,11 +15,13 @@ private char valedictorian;
 private double[] grades = new double[7]; 
 private String[] classes1 = new String[7]; 
 static boolean viewStudentLoop = true;
-static int currentGrade;
 static boolean addClassesLoop = true;
 static boolean mainClassLoop = true;
-   
+static boolean initialChange = false;
+static boolean afterChange = false; 
+static boolean afterChangeLoop = true;
 static boolean addStudentLoop = true;
+static int index;
 static Scanner input = new Scanner(System.in);
 
    // all classes students could take
@@ -76,6 +78,8 @@ static Scanner input = new Scanner(System.in);
     }
     
     public static void changeStudent(){
+        if (initialChange){
+            input.nextLine();
     // input will be put together for full name and email
         System.out.print("Student First Name: ");
         String answerFirstName = input.nextLine().trim();
@@ -101,7 +105,146 @@ static Scanner input = new Scanner(System.in);
                 school.get(i).id += school.size();   
      }
     }
+        initialChange = false;
    }
+        else if (afterChange){
+            afterChangeLoop = false;
+            input.nextLine();
+            System.out.println("Type The First, Middle Initial, And Last Name Of The Student");
+            String afterChangeName = input.nextLine();
+          
+            for (int p = 0; p < school.size(); p++){
+                if (school.get(p).name.equalsIgnoreCase(afterChangeName)){
+                    index = p; 
+                    students.afterChangeLoop = true;
+                }
+            }
+            
+            
+            
+            
+            
+            
+            
+            while (afterChangeLoop){
+               System.out.println("Type 1: View student\n"
+                       + "Type 2: Change Name\n"
+                       + "Type 3: Change Grade\n"
+                       + "Type 4: Change ID\n"
+                       + "Type 5: Change Absences\n"
+                       + "Type 6: Change honors\n"
+                       + "Type 7: Change Schedule\n"
+                       + "Type 8: Change Student Grades\n"
+                       + "Type 9: Quit Back To Main Menu");
+                
+               int afterChangeLoopAnswer = input.nextInt();
+               
+             switch (afterChangeLoopAnswer){
+                 
+                 case 1:
+                     viewStudent();
+                 break;
+                 case 2:
+                     input.nextLine();
+                      System.out.print("Student First Name: ");
+                      String answerFirstName = input.nextLine().trim();
+                      System.out.print("Student Middle Initial: ");
+                      String answerMiddleName = input.nextLine().trim();
+                      System.out.print("Student Last Name: ");
+                      String answerLastName = input.nextLine().trim();
+                     school.get(index).name = answerFirstName + " " + answerMiddleName + " " + answerLastName; 
+                     school.get(index).email = answerFirstName.substring(0, 1) + answerMiddleName.substring(0, 1) + answerLastName +  "@mail." + StudentManagementSystem.userSchoolName + ".edu";
+                     break;
+                 case 3:
+                     System.out.print("Change Grade(9,10,11,12): ");
+                     int gradeChange = input.nextInt();
+                     
+                     if (gradeChange <= 12 && gradeChange >= 9){
+                         school.get(index).grade = gradeChange;
+                     }
+                     else{
+                         System.out.println("Invalid Input...");
+                     }
+                     break;
+                 
+                 case 4:
+                     System.out.print("New ID(1000000-1999999): ");
+                     int newID = input.nextInt();
+                     
+                     if (newID >= 1000000 && newID <= 1999999){
+                         school.get(index).id = newID;
+                     }
+                     else {
+                         System.out.println("Invalid Input...");
+                     }
+                     break;
+                 case 5:
+                     System.out.println("Current Absenses: " + school.get(index).absences);
+                     System.out.print("New Number Of Absenses: ");
+                     int newAbsenses = input.nextInt();
+                     school.get(index).absences = newAbsenses;
+                     break;
+                 
+                 case 6: 
+                     System.out.println("Current Honors: " + school.get(index).honors);
+                     System.out.print("New Honors(Y/N): ");
+                     String inputNewHonors = input.nextLine().trim();
+                     
+                     if (inputNewHonors.equalsIgnoreCase("Y")){
+                      school.get(index).honors = 'Y';   
+                     }
+                     else if (inputNewHonors.equalsIgnoreCase("N")){
+                         school.get(index).honors = 'N';
+                     }
+                     else{
+                         System.out.println("Invalid Input...");
+                     }
+                     
+                     
+                     
+                     
+                     
+                     break;
+                 case 7:
+                    System.out.println("Available Classes: ");
+                    for (int g = 0; g < classes.size(); g++){
+                        System.out.println((g + 1) + ". " + classes.get(g));
+                    }
+                    System.out.println("Current Schedule");
+                    for (int t = 0; t < school.get(index).classes1.length; t++){
+                    System.out.println((t + 1) + ". " + school.get(index).classes1[t]);
+                        
+                    }
+                    System.out.print("Number Of Class To Change: ");
+                    int classToChange = input.nextInt();
+                    System.out.print("\nNew Class(Choose From Available Classes): ");
+                    int newClass = input.nextInt();
+                    school.get(index).classes1[classToChange - 1] = classes.get(newClass - 1);
+                     break;
+                 
+                 case 8:
+                  System.out.println(school.get(index).name + " | " + school.get(index).email + " | ID: " + school.get(index).id + "\n"
+                        + "Class            Grade\n"
+                        + "-----            -----");
+                for (int p = 0; p < school.get(index).classes1.length; p++){
+                   System.out.print((p + 1) + ". " + school.get(index).classes1[p]);
+                   System.out.println("             " + school.get(index).grades[p]);
+                }
+                System.out.print("Number Of Class To Change Grade In: ");
+                int changeGrade = input.nextInt();
+                System.out.print("New Grade: ");
+                double updatedGrade = input.nextDouble();
+                school.get(index).grades[changeGrade - 1] = updatedGrade;
+                 break;
+                 case 9:
+                 afterChangeLoop = false;
+                 afterChange = false;
+                 break;
+                 
+             }
+            }
+        }
+    }
     // adds student to system
     public static void addStudentToSchool(){
        students.school.add(addStudent());
@@ -239,6 +382,7 @@ static Scanner input = new Scanner(System.in);
     System.out.print("Type q to quit\n"
             + "Type Class To Add: ");
     String className = input.nextLine().trim();
+    // press q = quit
     if (className.equalsIgnoreCase("q") || className.equalsIgnoreCase("quit")){
         addClassesLoop = false;
         System.out.println("");
@@ -250,6 +394,7 @@ static Scanner input = new Scanner(System.in);
          break;
          
         case 3: 
+            // prints all classes with numbers
             for (int i = 0; i < classes.size(); i++){
             System.out.println((i + 1) + ". " + classes.get(i));
             }
