92bc322b5cebfa3193fddaf45be5538c1e4b7d49
kyle2 <kyle2@KylesLaptop>
2026-01-07 18:25:15 -0600

    First Menu & First Player Constructor

--- src/studentmanagementsystem/StudentManagementSystem.java
+++ src/studentmanagementsystem/StudentManagementSystem.java
@@ -1,8 +1,37 @@
 package studentmanagementsystem;
+import java.util.Scanner;
 public class StudentManagementSystem {
+static boolean menu = true;
 
     public static void main(String[] args) {
-    System.out.println("Hello"); 
-    }
+    Scanner input = new Scanner(System.in);
+    // intro
+    System.out.println("Welcome To ClassHelper, Your Application For Everything Student Management.");
     
+    // main menu
+    while (menu){
+    System.out.println("Type 1: View Grades\n"
+            + "Type 2: Change Student Info\n"
+            + "Type 3: Add Student\n"
+            + "Type 4: Remove Student Or Entire Grade\n"
+            + "Type 5: Change Year");
+    
+    int menuInput = input.nextInt();
+    
+    switch (menuInput){
+        case 1: 
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            break;
+   }
+  }
+ }
 }
---
+++ src/studentmanagementsystem/students.java
@@ -0,0 +1,39 @@
+package studentmanagementsystem;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class students {
+   String name;
+   int grade;
+   int overallGrade;
+   int id;
+   String email;
+   int attendence;
+   char honors;
+   char valedictorian;
+   
+   
+   
+   
+    public students(String studentName, int studentGrade, int studentOverallGrade, int studentID, String studentEmail, int studentAttendence, char studentHonors, char studentValedictorian){
+        this.name = studentName;
+        this.grade = studentGrade;
+        this.overallGrade = studentOverallGrade;
+        this.id = studentID;
+        this.email = studentEmail;
+        this.attendence = studentAttendence;
+        this.honors = studentHonors;
+        this.valedictorian = studentValedictorian;
+    }
+    public static void viewGrade(){
+        
+    }
+    public static void addStudent(){
+        
+    }
+    public static void removeStudent(){
+        
+    }
+    public static void changeYear(){
+        
+ }   
+}
