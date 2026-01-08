43274aee98eb36f2dcb283710cc498e023642ef4
kyle2 <kyle2@KylesLaptop>
2026-01-07 19:09:26 -0600

    Finished initial fields

--- src/studentmanagementsystem/students.java
+++ src/studentmanagementsystem/students.java
@@ -3,18 +3,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class students {
    String name;
-   int grade;
-   int overallGrade;
+   double grade;
+   String gradeLetter;
+   double overallGrade;
    int id;
    String email;
    int attendence;
    char honors;
    char valedictorian;
+   int grade1;
+   int grade2;
+   int grade3;
+   int grade4;
+   int grade5;
+   int grade6;
+   int grade7;
+   String class1;
+   String class2;
+   String class3;
+   String class4;
+   String class5;
+   String class6;
+   String class7;
    
+   // all classes students could take
+   ArrayList<String> classes = new ArrayList<String>();
+
+   // holds each grades students
+   ArrayList<students> freshmen = new ArrayList<>();
+   ArrayList<students> softmore = new ArrayList<>();
+   ArrayList<students> junior = new ArrayList<>();
+   ArrayList<students> senior = new ArrayList<>();
    
-   
-   
-    public students(String studentName, int studentGrade, int studentOverallGrade, int studentID, String studentEmail, int studentAttendence, char studentHonors, char studentValedictorian){
+    public students(String studentName, double studentGrade, String studentGradeLetter, double studentOverallGrade, int studentID, String studentEmail, int studentAttendence, char studentHonors, char studentValedictorian
+){
         this.name = studentName;
         this.grade = studentGrade;
         this.overallGrade = studentOverallGrade;
@@ -27,8 +49,7 @@ public class students {
     public static void viewGrade(){
         
     }
-    public static void addStudent(){
-        
+    public static void addStudent(){  
     }
     public static void removeStudent(){
         
